75cfcbb6dd262daf38cba53a14856f5d52d7ec0f
karina A <karina.arevalo@hotmail.com>
2022-09-14 18:26:03 -0500

    Fixing the color

---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<script setup>
+  import { ref } from 'vue'
+  const brand = ref('🏛️ Agency Records')
+</script>
+
+<template>
+  <nav>
+    <div class="Wrapper">
+      <div class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </div>
+      <div class="menu">
+        <a href="#" class="menu-item">Department</a>
+        <a href="#" class="menu-item">Settings</a>
+        <a href="#" class="menu-login">Logout</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="postcss">
+  nav {
+    @apply flex h-24  bg-yellow-900 text-rose-100;
+    .Wrapper {
+      @apply container mx-auto  flex w-full  items-center justify-between;
+      .brand {
+        &-title {
+          @apply text-3xl font-semibold text-red-300;
+        }
+      }
+      .menu {
+        @apply flex gap-4;
+        &-item {
+          @apply rounded-2xl px-3 py-3 hover:bg-red-600 hover:text-red-300;
+        }
+        &-login {
+          @apply rounded-2xl bg-red-600 px-3 py-3 text-rose-100 hover:bg-slate-500;
+        }
+      }
+    }
+  }
+</style>
