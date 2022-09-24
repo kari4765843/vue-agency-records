9a3ca340fc5f987b5d9ef2f8693e54b85d514d44
karina A <karina.arevalo@hotmail.com>
2022-09-23 21:17:44 -0500

    Added the vue router

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,9 @@
 <script setup>
   import { ref } from 'vue'
+  import { useAuth } from './composables/useAuth'
+
+  const { isAuthenticated, logout, user } = useAuth()
+
   const brand = ref('🏛️ Agency Records')
 </script>
 
@@ -10,9 +14,18 @@
         <span class="brand-title">{{ brand }}</span>
       </RouterLink>
       <div class="menu">
-        <a href="#" class="menu-item">Departments</a>
-        <a href="#" class="menu-item">Settings</a>
-        <a href="#" class="menu-login">Logout</a>
+        <p v-show="isAuthenticated" class="px-2 py-4">
+          Welcome back<strong
+            ><i>{{ user.name }}</i></strong
+          >
+        </p>
+        <div v-if="isAuthenticated">
+          <RouterLink :to="{ name: 'Settings' }" href="#" class="menu-item">Settings</RouterLink>
+          <Button class="menu-logout" @click="logout">Logout</Button>
+        </div>
+        <div v-else>
+          <RouterLink :to="{ name: 'Login' }" href="#" class="menu-login">Login</RouterLink>
+        </div>
       </div>
     </div>
   </nav>
@@ -29,12 +42,18 @@
         }
       }
       .menu {
-        @apply flex gap-4;
+        @apply flex gap-2;
+        & div {
+          @apply py-2;
+        }
         &-item {
-          @apply rounded-2xl px-3 py-3 hover:bg-blue-700 hover:text-red-300;
+          @apply rounded-md px-4 py-2 hover:bg-red-600 hover:text-red-300;
         }
         &-login {
-          @apply rounded-2xl bg-red-600 px-3 py-3 text-rose-100 hover:bg-slate-500;
+          @apply rounded-md bg-red-600 px-4 py-2 text-rose-100 hover:bg-yellow-800;
+        }
+        &-logout {
+          @apply rounded-md bg-blue-800 px-3 py-3 text-rose-100 hover:bg-yellow-800;
         }
       }
     }
--- src/router.js
+++ src/router.js
@@ -1,16 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuth } from '@/components/composables/useAuth'
+
+const { isAuthenticated } = useAuth()
 
 import MainPage from '@/components/MainPage.vue'
 import LoginPage from '@/components/LoginPage.vue'
+import SettingsPage from '@/components/SettingsPage.vue'
 import NotFound from '@/components/NotFound.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: MainPage },
   { path: '/login', name: 'Login', component: LoginPage },
+  { path: '/settings', name: 'Settings', component: SettingsPage, meta: { requiresAuth: true } },
   { path: '/:pathMatch(.*)*', name: 'Not Found', component: NotFound },
 ]
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+router.beforeEach((to, _, next) => {
+  if (to.meta.requiresAuth && !isAuthenticated.value) next({ name: 'Login', query: { redirect: to.fullPath } })
+  else next()
+})
+
+export default router
